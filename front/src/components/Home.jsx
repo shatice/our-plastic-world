a5c054c441eaa96a154aa09b907764463a7f8c2d
shatice <sarah.gungor@hetic.net>
2020-01-14 19:06:12 +0100

    test style

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
  
 const Home = () => {
     return(
-        <div>
-            <h1>HELLO</h1>
-            <Link to="/step1">Suivant</Link>
+        <div class="homeIntro">
+            <p>Les matières plastiques sont des ressources solides, durables et renouvelables. Pourtant leurs déchets représent  X% de la pollution mondiale....Découvre blablablablablablabla : </p>
+            <Link to="/step1"><button>C'est parti</button></Link>
         </div>
     );
 }
