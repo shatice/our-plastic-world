24d2be21d8896f12fef624f1760cf78471e4e54c
shatice <sarah.gungor@hetic.net>
2020-02-13 00:14:21 +0100

    add style to infos & nav contents

--- front/src/components/infos/Infos.jsx
+++ front/src/components/infos/Infos.jsx
@@ -4,34 +4,34 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 const Infos = () => {
   return(
     <div className="infos">
-      <section className="infos__production">
+      <section className="infos__bloc infos__bloc--production">
         <h2>PRODUCTION MONDIALE DE PLASTIQUE</h2>
         <ul>
-          <li>400 M. de Tonnes</li>
-          <li>3x plus qu’en 1980</li>
-          <li>7x plus que l’année précédente</li>
+          <li className="data">400 M. de Tonnes</li>
+          <li><strong>3x</strong> plus qu’en <strong>1980</strong></li>
+          <li><strong>7x</strong> plus que <strong>l’année précédente</strong></li>
         </ul>
       </section>
-      <section className="infos__management">
+      <section className="infos__bloc infos__bloc--management">
         <h2>GESTION DES DÉCHETS PLASTIQUES</h2>
         <ul>
           <li>
             <p>Recyclés</p>
-            <p>18%</p>
+            <p className="data">18%</p>
           </li>
           <li>
             <p>Incinérés</p>
-            <p>37%</p>
+            <p className="data">37%</p>
           </li>
           <li>
             <p>Dans la nature</p>
-            <p>45%</p>
+            <p className="data">45%</p>
           </li>
           <li>
             <p>Soit l’équivalent de</p>
-            <p>115</p>
-            <p>baleines bleues, </p>
-            <p>animal le plus lourd au monde</p>
+            <p className="data">115</p>
+            <strong>baleines bleues,</strong>
+            <p>l'animal le plus lourd du monde</p>
           </li>
         </ul>
       </section>
