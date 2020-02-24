e00bddc87e8e3d7c7465d8c39e1c5b2264bc836e
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-02-24 17:05:26 +0100

    set language switch

---
+++ front/src/components/legende/Legende.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/* STYLES */
+import '../legende/legende.scss';
+ 
+const Legende = () => {
+
+	return(
+    <div className="legende">
+        <p>Faible pollution</p>
+        <div className="linear"></div>
+        <p>Faible pollution</p>
+    </div>
+	);
+}
+   
+export default Legende;
