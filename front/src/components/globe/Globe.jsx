7f5ae6e5baf346e4d8b2c56e867d5ff1c73e0ced
shatice <sarah.gungor@hetic.net>
2020-02-26 01:12:14 +0100

    add heatrules to globe & remove i18n debug

--- front/src/components/globe/Globe.jsx
+++ front/src/components/globe/Globe.jsx
@@ -15,8 +15,11 @@ const Globe = () => {
 		// Apply Theme
 		am4core.useTheme(am4themes_animated);
 		let chart = am4core.create("chartdiv", am4maps.MapChart);
+
+		// C'est utile ?? 
 		let interfaceColors = new am4core.InterfaceColorSet();
 
+		// Set definition
 		try {
 			chart.geodata = am4geodata_worldLow;
 		}
@@ -24,10 +27,12 @@ const Globe = () => {
 			chart.raiseCriticalError(new Error("Map geodata could not be loaded. Please download the latest <a href=\"https://www.amcharts.com/download/download-v4/\">amcharts geodata</a> and extract its contents into the same directory as your amCharts files."));
 		}
 		
-		// Set projection
+		// Set projection : like 3d globe
 		chart.projection = new am4maps.projections.Orthographic();
+
+		// Set Behaviour
 		chart.panBehavior = "rotateLongLat";
-		// chart.deltaLatitude = -20;
+		chart.deltaLatitude = -20;
 		chart.padding(20,20,20,20);
 
 		// limits vertical rotation
@@ -106,11 +111,11 @@ const Globe = () => {
 				shadowPolygonSeries.raiseCriticalError(new Error("Map geodata could not be loaded. Please download the latest <a href=\"https://www.amcharts.com/download/download-v4/\">amcharts geodata</a> and extract its contents into the same directory as your amCharts files."));
 		}
 
-		// SHADOW
+		// Configure shadow TO SEE
 		shadowPolygonSeries.useGeodata = true;
 		shadowPolygonSeries.dx = 2;
 		shadowPolygonSeries.dy = 2;
-		shadowPolygonSeries.mapPolygons.template.fill = am4core.color("blue");
+		// shadowPolygonSeries.mapPolygons.template.fill = am4core.color("blue");
 		shadowPolygonSeries.mapPolygons.template.fillOpacity = 0.2;
 		shadowPolygonSeries.mapPolygons.template.strokeOpacity = 0;
 		shadowPolygonSeries.fillOpacity = 0.1;
@@ -122,15 +127,11 @@ const Globe = () => {
 
 		// Create map polygon series
 		let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+		// Load polygon data from GeoJSON (like country names)
 		polygonSeries.useGeodata = true;
 
+		// C'est quoi ??? 
 		polygonSeries.calculateVisualCenter = true;
-		polygonSeries.tooltip.background.fillOpacity = 0.2;
-		polygonSeries.tooltip.background.cornerRadius = 20;
-
-		let template = polygonSeries.mapPolygons.template;
-
-		template.nonScalingStroke = true;
 
 		// polygonSeries.calculateVisualCenter = true;
 		// template.propertyFields.id = "id";
@@ -199,125 +200,6 @@ const Globe = () => {
 
 		// 	})
 		// })
-
-		// let data = {
-		// 	"AL": 504.38,
-		// 	"AM": 6.5,
-		// 	"AO": 2.98,
-		// 	"AR": 0.32,
-		// 	"AT": 10.9,
-		// 	"AU": 5.02,
-		// 	"AZ": 17.38,
-		// 	"BA": 24.45,
-		// 	"BD": 13.4,
-		// 	"BE": 12.06,
-		// 	"BF": 93.37,
-		// 	"BG": 1.68,
-		// 	"BI": 0.95,
-		// 	"BJ": 93.36,
-		// 	"BR": 49.42,
-		// 	"BT": 10.03,
-		// 	"BY": 26.16,
-		// 	"CA": 0.96,
-		// 	"CD": 69.71,
-		// 	"CF": 4.57,
-		// 	"CG": 19.7,
-		// 	"CH": 6.19,
-		// 	"CI": 14.1,
-		// 	"CL": 1.4,
-		// 	"CM": 41.26,
-		// 	"CN": 2.6,
-		// 	"CO": 4.48,
-		// 	"CY": 7.69,
-		// 	"CZ": 23.09,
-		// 	"DK": 1.58,
-		// 	"EE": 9.91,
-		// 	"EG": 0.63,
-		// 	"ES": 4.96,
-		// 	"FI": 3.27,
-		// 	"FR": 43.26,
-		// 	"GA": 3.03,
-		// 	"GB": 14.3,
-		// 	"GE": 809.09,
-		// 	"GH": 39.78,
-		// 	"GM": 2.45,
-		// 	"GN": 45.98,
-		// 	"GQ": 23.74,
-		// 	"GR": 154.42,
-		// 	"HR": 5.46,
-		// 	"HU": 1.44,
-		// 	"ID": 16.87,
-		// 	"IE": 17.56,
-		// 	"IL": 412.24,
-		// 	"IN": 47.85,
-		// 	"IQ": 12.96,
-		// 	"IR": 1.13,
-		// 	"IT": 44.29,
-		// 	"JP": 3.27,
-		// 	"KE": 16.8,
-		// 	"KG": 253.37,
-		// 	"KH": 0.44,
-		// 	"KM": 1.26,
-		// 	"KZ": 116.3,
-		// 	"LA": 1.33,
-		// 	"LK": 0.53,
-		// 	"LR": 692.27,
-		// 	"LS": 5.9,
-		// 	"LT": 14.44,
-		// 	"LU": 6.95,
-		// 	"LV": 6.09,
-		// 	"MA": 0.2,
-		// 	"MD": 83.75,
-		// 	"ME": 319.75,
-		// 	"MG": 2386.35,
-		// 	"MK": 28.83,
-		// 	"ML": 48.68,
-		// 	"MM": 40.31,
-		// 	"MN": 0.66,
-		// 	"MR": 14.65,
-		// 	"MT": 11.65,
-		// 	"MV": 9.35,
-		// 	"MX": 0.04,
-		// 	"MY": 86.41,
-		// 	"MZ": 13.49,
-		// 	"NA": 12.9,
-		// 	"NE": 80.88,
-		// 	"NG": 31.44,
-		// 	"NL": 1.47,
-		// 	"NO": 2.47,
-		// 	"NP": 10.8,
-		// 	"NZ": 9.23,
-		// 	"PE": 1.29,
-		// 	"PK": 159.14,
-		// 	"PL": 8.24,
-		// 	"PT": 16.68,
-		// 	"RO": 63.05,
-		// 	"RS": 473.46,
-		// 	"RU": 14.24,
-		// 	"RW": 5.45,
-		// 	"SE": 2.64,
-		// 	"SG": 8.18,
-		// 	"SI": 3.37,
-		// 	"SK": 112.78,
-		// 	"SN": 3.37,
-		// 	"SO": 8.03,
-		// 	"SS": 19.3,
-		// 	"TD": 75.63,
-		// 	"TG": 34.84,
-		// 	"TH": 81.02,
-		// 	"TL": 9.46,
-		// 	"TN": 7.8,
-		// 	"TR": 7.08,
-		// 	"UA": 1439.02,
-		// 	"UG": 62.55,
-		// 	"US": 1.32,
-		// 	"UZ": 0.99,
-		// 	"VE": 179.55,
-		// 	"ZA": 3.09,
-		// 	"ZM": 9.82,
-		// 	"ZW": 0.06
-		// }
-
 		// template.fill = am4core.color("red");
 
 		// console.log(data.AL); 
@@ -334,16 +216,14 @@ const Globe = () => {
 		// template.stroke = am4core.color("black");
 
 		// IMPORTANT DESKOP -- Hover;
-		let hs = polygonSeries.mapPolygons.template.states.create("hover");
+		// let hs = polygonSeries.mapPolygons.template.states.create("hover");
 		// hs.properties.fillOpacity = 1;
 		// hs.properties.fill = am4core.color("blue");
 
-		template.events.on("hit", function (event) {
-			console.log(event.target.dataItem.dataContext.name);
-			console.log(event.target.dataItem.dataContext);
-			console.log(event.target.dataItem.dataContext.id);
-			console.log(event.target.dataItem.dataContext);
-			console.log(event.target.dataItem.dataContext);
+		// polygonTemplate.events.on("hit", function (event) {
+			// console.log(event.target.dataItem.dataContext.name);
+			// console.log(event.target.dataItem.dataContext);
+			// console.log(event.target.dataItem.dataContext.id);
 			// event.target.properties.fill = am4core.color("blue"); 
 			// console.log(event.target.properties.fill);
 			// console.log('okk');
@@ -351,16 +231,16 @@ const Globe = () => {
 			// hs.properties.fillOpacity = .2; 
 			// hs.properties.fill= am4core.color("blue");
 			// console.log(hs.properties.fillOpacity);
-		})
+		// })
 
 		// template.fill = am4core.color("#74B266");
-		template.events.on("hit", function(event) {
+		// polygonTemplate.events.on("hit", function(event) {
 			// ev.target.series.chart.zoomToMapObject(ev.target);
-			console.log('ooooookkkkkk'); 
-			console.log(template); 
-			event.target.fill = am4core.color("#fff");
-			template.fill = am4core.color("#74B266");
-		})
+			// console.log('ooooookkkkkk'); 
+			// console.log(template); 
+			// event.target.fill = am4core.color("#fff");
+			// polygonTemplate.fill = am4core.color("#74B266");
+		// })
 
 		// selectPolygon(event.target);
 		// event.target.zIndex = 1000000;
@@ -369,6 +249,205 @@ const Globe = () => {
 		// setTimeout(function(){
 		// 	animation = chart.animate({property:"deltaLongitude", to:100000}, 20000000);
 		// })
+
+		// Configure series
+		let polygonTemplate = polygonSeries.mapPolygons.template;
+		polygonTemplate.nonScalingStroke = true;
+			
+		console.log(polygonSeries.mapPolygons.values);
+
+		// Configure tooltips : country name
+		polygonTemplate.tooltipText = "{name}";
+		// Set tooltip style
+		polygonSeries.tooltip.background.fillOpacity = .5;
+		polygonSeries.tooltip.background.cornerRadius = 30;
+
+		polygonSeries.heatRules.push({
+			"property": "fill",
+			"target": polygonTemplate,
+			"min": am4core.color("#FFEBB2"),
+			"max": am4core.color("#C00B0B")
+		});
+
+		var hs = polygonTemplate.states.create("hover");
+		hs.properties.fill = am4core.color("#fff");
+
+		polygonSeries.data = [
+			{ id: "AF", value: 60.524 },
+			{ id: "AL", value: 77.185 },
+			{ id: "DZ", value: 70.874 },
+			{ id: "AO", value: 51.498 },
+			{ id: "AR", value: 76.128 },
+			{ id: "AM", value: 74.469 },
+			{ id: "AU", value: 82.364 },
+			{ id: "AT", value: 80.965 },
+			{ id: "AZ", value: 70.686 },
+			{ id: "BH", value: 76.474 },
+			{ id: "BD", value: 70.258 },
+			{ id: "BY", value: 69.829 },
+			{ id: "BE", value: 80.373 },
+			{ id: "BJ", value: 59.165 },
+			{ id: "BT", value: 67.888 },
+			{ id: "BO", value: 66.969 },
+			{ id: "BA", value: 76.211 },
+			{ id: "BW", value: 47.152 },
+			{ id: "BR", value: 73.667 },
+			{ id: "BN", value: 78.35 },
+			{ id: "BG", value: 73.448 },
+			{ id: "BF", value: 55.932 },
+			{ id: "BI", value: 53.637 },
+			{ id: "KH", value: 71.577 },
+			{ id: "CM", value: 54.61 },
+			{ id: "CA", value: 81.323 },
+			{ id: "CV", value: 74.771 },
+			{ id: "CF", value: 49.517 },
+			{ id: "TD", value: 50.724 },
+			{ id: "CL", value: 79.691 },
+			{ id: "CN", value: 75.178 },
+			{ id: "CO", value: 73.835 },
+			{ id: "KM", value: 60.661 },
+			{ id: "CD", value: 49.643 },
+			{ id: "CG", value: 58.32 },
+			{ id: "CR", value: 79.712 },
+			{ id: "CI", value: 50.367 },
+			{ id: "HR", value: 76.881 },
+			{ id: "CU", value: 79.088 },
+			{ id: "CY", value: 79.674 },
+			{ id: "CZ", value: 77.552 },
+			{ id: "DK", value: 79.251 },
+			{ id: "GL", value: 79.251 },
+			{ id: "DJ", value: 61.319 },
+			{ id: "DO", value: 73.181 },
+			{ id: "EC", value: 76.195 },
+			{ id: "EG", value: 70.933 },
+			{ id: "SV", value: 72.361 },
+			{ id: "GQ", value: 52.562 },
+			{ id: "ER", value: 62.329 },
+			{ id: "EE", value: 74.335 },
+			{ id: "ET", value: 62.983 },
+			{ id: "FJ", value: 69.626 },
+			{ id: "FI", value: 80.362 },
+			{ id: "FR", value: 81.663 },
+			{ id: "GA", value: 63.115 },
+			{ id: "GF", value: 79.9 },
+			{ id: "GM", value: 58.59 },
+			{ id: "GE", value: 74.162 },
+			{ id: "DE", value: 80.578 },
+			{ id: "GH", value: 60.979 },
+			{ id: "GR", value: 80.593 },
+			{ id: "GT", value: 71.77 },
+			{ id: "GN", value: 55.865 },
+			{ id: "GW", value: 54.054 },
+			{ id: "GY", value: 66.134 },
+			{ id: "HT", value: 62.746 },
+			{ id: "HN", value: 73.503 },
+			{ id: "HK", value: 83.199 },
+			{ id: "HU", value: 74.491 },
+			{ id: "IS", value: 81.96 },
+			{ id: "IN", value: 66.168 },
+			{ id: "ID", value: 70.624 },
+			{ id: "IR", value: 73.736 },
+			{ id: "IQ", value: 69.181 },
+			{ id: "IE", value: 80.531 },
+			{ id: "IL", value: 81.641 },
+			{ id: "IT", value: 82.235 },
+			{ id: "JM", value: 73.338 },
+			{ id: "JP", value: 83.418 },
+			{ id: "JO", value: 73.7 },
+			{ id: "KZ", value: 66.394 },
+			{ id: "KE", value: 61.115 },
+			{ id: "KP", value: 69.701 },
+			{ id: "KR", value: 81.294 },
+			{ id: "KW", value: 74.186 },
+			{ id: "KG", value: 67.37 },
+			{ id: "LA", value: 67.865 },
+			{ id: "LV", value: 72.045 },
+			{ id: "LB", value: 79.716 },
+			{ id: "LS", value: 48.947 },
+			{ id: "LR", value: 60.23 },
+			{ id: "LY", value: 75.13 },
+			{ id: "LT", value: 71.942 },
+			{ id: "LU", value: 80.371 },
+			{ id: "MK", value: 75.041 },
+			{ id: "MG", value: 64.28 },
+			{ id: "MW", value: 54.798 },
+			{ id: "MY", value: 74.836 },
+			{ id: "ML", value: 54.622 },
+			{ id: "MR", value: 61.39 },
+			{ id: "MU", value: 73.453 },
+			{ id: "MX", value: 77.281 },
+			{ id: "MD", value: 68.779 },
+			{ id: "MN", value: 67.286 },
+			{ id: "ME", value: 74.715 },
+			{ id: "MA", value: 70.714 },
+			{ id: "EH", value: 70.714 },
+			{ id: "MZ", value: 49.91 },
+			{ id: "MM", value: 65.009 },
+			{ id: "NA", value: 64.014 },
+			{ id: "NP", value: 67.989 },
+			{ id: "NL", value: 80.906 },
+			{ id: "NZ", value: 80.982 },
+			{ id: "NI", value: 74.515 },
+			{ id: "NE", value: 57.934 },
+			{ id: "NG", value: 52.116 },
+			{ id: "NO", value: 81.367 },
+			{ id: "SJ", value: 81.367 },
+			{ id: "OM", value: 76.287 },
+			{ id: "PK", value: 66.42 },
+			{ id: "PA", value: 77.342 },
+			{ id: "PG", value: 62.288 },
+			{ id: "PY", value: 72.181 },
+			{ id: "PE", value: 74.525 },
+			{ id: "PH", value: 68.538 },
+			{ id: "PL", value: 76.239 },
+			{ id: "PT", value: 79.732 },
+			{ id: "QA", value: 78.231 },
+			{ id: "RO", value: 73.718 },
+			{ id: "RU", value: 67.874 },
+			{ id: "RW", value: 63.563 },
+			{ id: "SA", value: 75.264 },
+			{ id: "SN", value: 63.3 },
+			{ id: "RS", value: 73.934 },
+			{ id: "SL", value: 45.338 },
+			{ id: "SG", value: 82.155 },
+			{ id: "SK", value: 75.272 },
+			{ id: "SI", value: 79.444 },
+			{ id: "SB", value: 67.465 },
+			{ id: "SO", value: 54 },
+			{ id: "ZA", value: 56.271 },
+			{ id: "SS", value: 54.666 },
+			{ id: "ES", value: 81.958 },
+			{ id: "LK", value: 74.116 },
+			{ id: "SD", value: 61.875 },
+			{ id: "SR", value: 70.794 },
+			{ id: "SZ", value: 48.91 },
+			{ id: "SE", value: 81.69 },
+			{ id: "CH", value: 82.471 },
+			{ id: "SY", value: 71 },
+			{ id: "TW", value: 79.45 },
+			{ id: "TJ", value: 67.118 },
+			{ id: "TZ", value: 60.885 },
+			{ id: "TH", value: 74.225 },
+			{ id: "TL", value: 67.033 },
+			{ id: "TG", value: 56.198 },
+			{ id: "TT", value: 69.761 },
+			{ id: "TN", value: 75.632 },
+			{ id: "TR", value: 74.938 },
+			{ id: "TM", value: 65.299 },
+			{ id: "UG", value: 58.668 },
+			{ id: "UA", value: 68.414 },
+			{ id: "AE", value: 76.671 },
+			{ id: "GB", value: 80.396 },
+			{ id: "US", value: 78.797 },
+			{ id: "UY", value: 77.084 },
+			{ id: "UZ", value: 68.117 },
+			{ id: "VE", value: 74.477 },
+			{ id: "PS", value: 73.018 },
+			{ id: "VN", value: 75.793 },
+			{ id: "YE", value: 62.923 },
+			{ id: "ZM", value: 57.037 },
+			{ id: "ZW", value: 58.142 }
+		];
 		
 	}, [])
 	
