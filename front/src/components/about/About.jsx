82d1c85ea31a416395f510c590280ee5d8f8b482
shatice <sarah.gungor@hetic.net>
2020-02-27 23:59:16 +0100

    set about

--- front/src/components/about/About.jsx
+++ front/src/components/about/About.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import './about.scss';
 import Goabout from '../goabout/Goabout.jsx';
+import { useTranslation } from 'react-i18next';
 
 const About = () => {
+    const { t } = useTranslation();
+
     return(
         <div className="background__about">
-        <div className="about">
-            <h2 className="second__title">À PROPOS</h2>
-            <h3 className="third__title">Une visualisation interactive des données de pollution plastique</h3>
-            <p className="first__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu est orci quisque fames. Elementum in viverra vel mollis vitae eget bibendum duis elit. Praesent lorem est eget eget enim pellentesque rhoncus nunc, morbi. Iaculis blandit vel nunc pharetra sed magna orci.</p>
-            <div className="container__four--one">
-                <div className="one__text">
-                    <h4 className="four__title">Lorem ipsum</h4>
-                    <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu est orci quisque fames. Elementum in viverra vel mollis vitae eget bibendum duis elit</p>
+            <div className="about">
+                <h2 className="second__title">{t('about.about-title')}</h2>
+                <h3 className="third__title">{t('about.about-subtitle')}</h3>
+                <p className="first__text">{t('about.about-desc')}</p>
+                <div className="container__four--one">
+                    <div className="one__text">
+                        <h4 className="four__title">{t('about.evol-title')}</h4>
+                        <p>{t('about.evol-content-1')}</p>
+                        <p>{t('about.evol-content-2')}</p>
+                        <p>{t('about.evol-content-3')}</p>
+                    </div>
+                    <div className="one__text">
+                        <h4 className="four__title">{t('about.repartition-title')}</h4>
+                        <p>{t('about.repartition-content-1')}</p>
+                        <p>{t('about.repartition-content-2')}</p>
+                    </div>
                 </div>
-                <div className="one__text">
-                    <h4 className="four__title">Lorem ipsum</h4>
-                    <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu est orci quisque fames. Elementum in viverra vel mollis vitae eget bibendum duis elit</p>
+                <div className="container__four--one">
+                    <div className="one__text">
+                        <h4 className="four__title">{t('about.consequences-title')}</h4>
+                        <p>{t('about.consequences-content-1')}</p>
+                        <p>{t('about.consequences-content-2')}</p>
+                        <p>{t('about.consequences-content-3')}</p>
+                    </div>
+                    <div className="one__text">
+                        <h4 className="four__title">{t('about.responsability-title')}</h4>
+                        <p>{t('about.responsability-content-1')}</p>
+                        <p>{t('about.responsability-content-2')}</p>
+                    </div>
                 </div>
+                <h2 className="second__title">{t('about.sources-title')}</h2>
+                {t('about.sources-content')}
             </div>
-            <div className="container__four--two">
-                <div className="one__text">
-                    <h4 className="four__title">Lorem ipsum</h4>
-                    <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu est orci quisque fames. Elementum in viverra vel mollis vitae eget bibendum duis elit</p>
-                </div>
-                <div className="one__text">
-                    <h4 className="four__title">Lorem ipsum</h4>
-                    <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu est orci quisque fames. Elementum in viverra vel mollis vitae eget bibendum duis elit</p>
-                </div>
-            </div>
-            <h2 className="second__title">SOURCES</h2>
-            <p className="text">Our World in Data - Plastic Pollution </p>
-        </div>
         </div>
         
 
