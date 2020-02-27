21db93d5de8e58cac2c4d37fc8a92a95dfb6b0d2
shatice <sarah.gungor@hetic.net>
2020-02-28 00:20:08 +0100

    add intro content

--- front/src/routes/intro/Intro.jsx
+++ front/src/routes/intro/Intro.jsx
@@ -1,20 +1,24 @@
 import React, {} from "react";
 import { Link } from "react-router-dom";
 import './intro.scss';
+import { useTranslation } from 'react-i18next';
 
 /***** COMPONENTS *****/
 import Header from '../../components/header/Header.jsx';
 import Globe from '../../components/globe/Globe.jsx';
  
 const Intro = () => {
+    const { t } = useTranslation();
 
     return(
         <div className="intro">
             <Header/>
             <section className="intro__content">
                 <h1>Our Plastic World</h1>
-                <p>La production de matières plastiques ne cesse d’augmenter depuis 1950, elle fait partie de notre quotidien. Le plastique est solide et recyclable, pourtant il représente X% de la pollution mondiale...</p>
-                <Link to="/main"><button className="cta">C'est parti</button></Link>
+                <p>{t('intro.1')}</p>
+                <p>{t('intro.2')}</p>
+                <p>{t('intro.3')}</p>
+                <Link to="/main"><button className="cta">{t('intro.cta')}</button></Link>
             </section>
             <Globe/>
         </div>
