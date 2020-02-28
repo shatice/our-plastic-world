c5105c0bbd871ab1fba6c8b68d6567fecf26c2ec
HETIC <rami.cakir01@gmail.com>
2020-02-28 09:43:29 +0100

    permute

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,57 +1,75 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useTranslation } from 'react-i18next';
-import Sound from 'react-sound';
-import soundfile from '../../assets/audio/sound.wav';
-import soundClick from '../../assets/audio/click.mp3';
-import './header.scss';
+import { useTranslation } from "react-i18next";
+import Sound from "react-sound";
+import soundfile from "../../assets/audio/sound.wav";
+import soundClick from "../../assets/audio/click.mp3";
+import "./header.scss";
 
 const Header = () => {
-  
   const [swapSound, setSwapSound] = useState(Sound.status.STOPPED);
   const [swapClick, setSwapClick] = useState(Sound.status.STOPPED);
-  
+
   const { t, i18n } = useTranslation();
-  
-  function changeLanguage(lang){
+
+  function changeLanguage(lang) {
     i18n.changeLanguage(lang);
   }
 
   const toggle = () => {
     if (swapSound === Sound.status.PLAYING) {
       setSwapSound(Sound.status.STOPPED);
-    }else {
+    } else {
       setSwapSound(Sound.status.PLAYING);
     }
-    
-  }
+  };
   const add = () => {
     if (swapClick === Sound.status.STOPPED) {
       setSwapClick(Sound.status.PLAYING);
     }
-  }
+  };
 
-  return(
+  return (
     <header className="header">
-      <Sound url={soundfile} playStatus={swapSound} loop={true}/>
+      <Sound url={soundfile} playStatus={swapSound} loop={true} />
       <Sound url={soundClick} playStatus={swapClick} />
       <nav className="navIcons">
         <ul className="navList">
           <div onClick={add} className="nav__language">
-            <li onClick={() => {changeLanguage('en')}} style={{opacity: i18n.language === "en" ? "1" : "0.5"}} className={`navItem navItem--lang`}>EN</li>
-            <li onClick={() => {changeLanguage('fr')}} style={{opacity: i18n.language === "fr" ? "1" : "0.5"}} className={`navItem navItem--lang`}>FR</li>
+            <li
+              onClick={() => {
+                changeLanguage("fr");
+              }}
+              style={{ opacity: i18n.language === "fr" ? "1" : "0.5" }}
+              className={`navItem navItem--lang`}
+            >
+              FR
+            </li>
+            <li
+              onClick={() => {
+                changeLanguage("en");
+              }}
+              style={{ opacity: i18n.language === "en" ? "1" : "0.5" }}
+              className={`navItem navItem--lang`}
+            >
+              EN
+            </li>
           </div>
           <li onClick={toggle} className="navItem navItem--audio">
-            <svg className="navItem__icon navItem__icon--audioOff"><use xlinkHref="/sprite-sheet.svg#audioOffIcon" /></svg>
+            <svg className="navItem__icon navItem__icon--audioOff">
+              <use xlinkHref="/sprite-sheet.svg#audioOffIcon" />
+            </svg>
           </li>
           <li onClick={add} className="navItem navItem--turnOff">
             <Link to="/">
-              <svg className="navItem__icon navItem__icon--turnOff"><use xlinkHref="/sprite-sheet.svg#turnOffIcon" /></svg>
+              <svg className="navItem__icon navItem__icon--turnOff">
+                <use xlinkHref="/sprite-sheet.svg#turnOffIcon" />
+              </svg>
             </Link>
           </li>
         </ul>
       </nav>
     </header>
   );
-}
+};
 export default Header;
--- front/src/components/nav/Nav.jsx
+++ front/src/components/nav/Nav.jsx
@@ -1,48 +1,48 @@
 import React, { useState } from "react";
 import views from "../../js/models/views.js";
-import { useTranslation } from 'react-i18next';
-import './nav.scss';
- 
+import { useTranslation } from "react-i18next";
+import "./nav.scss";
+
 const Nav = ({ setStateInfos }) => {
-	const { t } = useTranslation();
+  const { t } = useTranslation();
 
-	const [elementDashboard, setElementDashboard] = useState();
+  const [elementDashboard, setElementDashboard] = useState(0);
 
-	const dashboard = 
-	[
-		{
-			mode: views.Global, 
-			label: t('nav.1')
-		},
-		{
-			mode: views.Continents, 
-			label: t('nav.2')
-		},
-		{
-			mode: views.Countries,
-			label: t('nav.3')
-		}
-	]
+  const dashboard = [
+    {
+      mode: views.Global,
+      label: t("nav.1")
+    },
+    {
+      mode: views.Continents,
+      label: t("nav.2")
+    },
+    {
+      mode: views.Countries,
+      label: t("nav.3")
+    }
+  ];
 
-	return(
-		<nav className="nav">
-			<ul className="nav__items">
-				{ dashboard.map((d, index) => 
-				<li 
-					onClick={ () => { 
-						setStateInfos(d.mode); 
-						setElementDashboard(index)
-					}} 
-					style={
-						elementDashboard === index ? {opacity: 1} : {opacity: 0.5}
-					}
-					key={index}>
-					{d.label}
-				</li> 
-				)}
-			</ul>
-		</nav>
-	);
-}
-   
+  return (
+    <nav className="nav">
+      <ul className="nav__items">
+        {dashboard.map((d, index) => (
+          <li
+            onClick={() => {
+              setStateInfos(d.mode);
+              setElementDashboard(index);
+            }}
+            style={
+              elementDashboard === index ? { opacity: 1 } : { opacity: 0.5 }
+            }
+            key={index}
+          >
+            {d.label}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
 export default Nav;
